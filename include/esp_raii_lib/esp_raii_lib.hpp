550d212170aa721d1599b9507be9b6431ad472e8
Martin Boros <mboros04@gmail.com>
2025-08-23 13:45:47 -0400

    fix formatting issues

--- include/esp_raii_lib/esp_raii_lib.hpp
+++ include/esp_raii_lib/esp_raii_lib.hpp
@@ -1,3 +1,17 @@
+// Copyright 2024 ESP RAII Library Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 // Main header file for ESP RAII Library
@@ -12,5 +26,5 @@
 #define ESP_RAII_LIB_VERSION_PATCH 0
 
 namespace esp_raii {
-    constexpr const char* version = "0.1.0";
-}
+constexpr const char* version = "0.1.0";
+}  // namespace esp_raii
--- include/esp_raii_lib/gpio.hpp
+++ include/esp_raii_lib/gpio.hpp
@@ -1,3 +1,17 @@
+// Copyright 2024 ESP RAII Library Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include "driver/gpio.h"
@@ -6,27 +20,27 @@
 namespace esp_raii {
 
 class GPIO {
-public:
-    explicit GPIO(gpio_num_t pin) 
+ public:
+    explicit GPIO(gpio_num_t pin)
         : pin_(pin), configured_(false) {
     }
-    
+
     ~GPIO() {
         if (configured_) {
             gpio_reset_pin(pin_);
         }
     }
-    
+
     // Delete copy constructor and copy assignment
     GPIO(const GPIO&) = delete;
     GPIO& operator=(const GPIO&) = delete;
-    
+
     // Enable move constructor and move assignment
-    GPIO(GPIO&& other) noexcept 
+    GPIO(GPIO&& other) noexcept
         : pin_(other.pin_), configured_(other.configured_) {
         other.configured_ = false;
     }
-    
+
     GPIO& operator=(GPIO&& other) noexcept {
         if (this != &other) {
             if (configured_) {
@@ -38,7 +52,7 @@ public:
         }
         return *this;
     }
-    
+
     esp_err_t configure_output() {
         esp_err_t ret = gpio_set_direction(pin_, GPIO_MODE_OUTPUT);
         if (ret == ESP_OK) {
@@ -46,7 +60,7 @@ public:
         }
         return ret;
     }
-    
+
     esp_err_t configure_input(gpio_pull_mode_t pull_mode = GPIO_FLOATING) {
         esp_err_t ret = gpio_set_direction(pin_, GPIO_MODE_INPUT);
         if (ret == ESP_OK) {
@@ -57,20 +71,20 @@ public:
         }
         return ret;
     }
-    
+
     esp_err_t set_level(uint32_t level) {
         return gpio_set_level(pin_, level);
     }
-    
+
     int get_level() {
         return gpio_get_level(pin_);
     }
-    
+
     gpio_num_t pin() const { return pin_; }
-    
-private:
+
+ private:
     gpio_num_t pin_;
     bool configured_;
 };
 
-} // namespace esp_raii
+}  // namespace esp_raii
